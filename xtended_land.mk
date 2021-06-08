5cf081c7199181f871f8812d83a9e3d52ae07cc5
Aadi-ngp <84327664+Aadi2002@users.noreply.github.com>
2021-06-08 23:23:26 +0530

    Update xtended_land.mk

--- xtended_land.mk
+++ xtended_land.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 TARGET_ARCH := arm64
 
 # Bootanimation
-TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
